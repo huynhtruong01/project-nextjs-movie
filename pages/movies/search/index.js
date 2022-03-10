15ec2aa3a30b0094541a9d28d990e666f6d2c4eb
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2022-03-10 13:55:09 +0700

    update pages

---
+++ pages/movies/search/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+SearchMovie.propTypes = {}
+
+function SearchMovie(props) {
+  return <div></div>
+}
+
+export default SearchMovie
---
+++ pages/tv-shows/search/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+SearchTV.propTypes = {}
+
+function SearchTV(props) {
+  return <div></div>
+}
+
+export default SearchTV
